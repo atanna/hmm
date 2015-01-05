2ff55b02d43ec68726e081f64a4d1d7874867b9f
atanna <anne.atamanova@gmail.com>
2015-01-05 18:57:40 +0400

    changed structure

--- hmm_/hmm.py
+++ hmm_/hmm.py
@@ -53,14 +53,14 @@ class HMMModel():
             h_state = stats.rv_discrete(name='custm',
                                         values=(hidden_states, p)).rvs()
             sample[i] = stats.rv_discrete(name='custm',
-                                          values=(states, self.b[h_state])).rvs()
+                                          values=(
+                                          states, self.b[h_state])).rvs()
             p = self.a[h_state]
         return sample
 
     def distance(self, model):
         return np.mean([abs(self.a - model.a).mean(),
-                       abs(self.b - model.b).mean(),
-                       abs(self.pi - model.pi).mean()])
+                        abs(self.b - model.b).mean()])
 
     def __str__(self):
         return "a: {}\n" \
@@ -75,24 +75,18 @@ class HMM():
         """
         self.n = n
 
-    def observation_probability(self, model, data):
+    @staticmethod
+    def observation_probability(model, data):
         """
-        Forward-backward procedure
-        additional values: alpha
-        alpha[t,i] = p(data[1]...data[t]| q_t=x_i, model)
         :param model:
         :param data:
         :return: p(data| model)
 
         """
-        alpha_t = model.pi * model.b[:, data[0]]
+        return HMM.forward(model, data)[-1].sum()
 
-        for d in data[1:]:
-            alpha_t = alpha_t.dot(model.a) * (model.b[:, d])
-
-        return alpha_t.sum()
-
-    def optimal_state_sequence(self, model, data):
+    @staticmethod
+    def optimal_state_sequence(model, data):
         """
         iteration optimal algorithm
         additional values: gamma, delta, psi
@@ -116,70 +110,83 @@ class HMM():
         T = len(data)
         psi = np.array(psi)
         q = np.zeros(T)
-        q[T-1] = delta_t.argmax()
-        for i in range(2, T+1):
-            q[-i] = psi[-i+1][q[-i + 1]]
+        q[T - 1] = delta_t.argmax()
+        for i in range(2, T + 1):
+            q[-i] = psi[-i + 1][q[-i + 1]]
         return q.astype(np.int)
+
+    @staticmethod
+    def forward(model, data):
+        """
+        Forward procedure
+        additional values: alpha
+        alpha[t,i] = p(data[1]...data[t]| q_t=x_i, model)
+        :param model:
+        :param data:
+        :return: alpha
+        """
+        T = len(data)
+        alpha = np.zeros((T, model.n))
+
+        alpha[0] = model.pi * model.b[:, data[0]]
+        for t in range(1, T):
+            alpha[t] = alpha[t - 1].dot(model.a) * (model.b[:, data[t]])
+        return alpha
+
+    @staticmethod
+    def backward(model, data):
+        """
+        Backward procedure
+        additional values: beta
+        beta[t,i] = p(data[t+1]...data[T]| q_t=x_i, model)
+        :param model:
+        :param data:
+        :return: beta
+        """
+        T = len(data)
+        beta = np.zeros((T, model.n))
+        beta[T - 1] = 1.
+        for t in range(T - 2, -1, -1):
+            beta[t] = model.a.dot(model.b[:, data[t + 1]] * beta[t + 1])
+        return beta
 
     def _optimal_model(self, data, start_model=None, eps=1e-30, max_iter=1e5):
         """
         Baum-Welch algorithm
         p(data| model) ->_model max
         (it finds !!!local maximum..)
-        additional values: ksi, gamma
-        ksi_t[i,j] = p(q_t=x_i, q_(t+1)=x_j|data, model)
         :param data:
         :return: optimal hmm_model
         """
 
-        def em(p, eps_check = 1e-5):
-            nonlocal model
-            beta = np.zeros((T, n))
+        def update_params(alpha, beta):
+            """
+            additional values: ksi, gamma
+            ksi_t[i,j] = p(q_t=x_i, q_(t+1)=x_j|data, model)
+            gamma = p(q_t = x_i| D, alpha)
+            :param alpha:
+            :param beta:
+            :return:
+            """
             a = np.zeros((n, n))
             b = np.zeros((n, m))
-            pi = np.zeros(n)
-            gamma_t = np.zeros(n)
-            ksi_t = None
+            gamma = np.zeros((T, n))
 
-            beta[T - 1] = 1.
-            for t in range(T - 2, -1, -1):
-                beta[t] = model.a.dot(model.b[:, data[t + 1]] * beta[t + 1])
+            p = alpha[-1].sum()
+            for t in range(T - 1):
+                ksi_t = alpha[t][:, np.newaxis] * model.a \
+                        * model.b[:, data[t + 1]] * beta[t + 1] / p
+                gamma[t] = ksi_t.sum(axis=1)
 
-
-            def do(t):
-                nonlocal a, b, ksi_t, gamma_t, alpha_t
-                ksi_t = alpha_t[:, np.newaxis] * model.a \
-                    * model.b[:, data[t+1]] * beta[t+1] / p
-
-                gamma_t = ksi_t.sum(axis=1)
-                b[:, data[t]] += gamma_t
                 a += ksi_t
-                assert abs(alpha_t.dot(beta[t]) - p) < eps_check, \
-                    "{} != p".format(alpha_t.dot(beta[t]))
-                assert abs(gamma_t.sum() - 1.) < eps_check, \
-                    "{} != 1.".format(gamma_t.sum())
-                assert abs((alpha_t * beta[t] / p).sum() - 1.) < eps_check, \
-                    "{} != 1.".format((alpha_t * beta[t] / p).sum())
+                b[:, data[t]] += gamma[t]
 
-            alpha_t = model.pi * model.b[:, data[0]]
-            do(0)
-            pi = gamma_t.copy()
-            sum_gamma = gamma_t.copy()
+            sum_gamma = gamma.sum(axis=0)[:, np.newaxis]
+            a /= sum_gamma
+            b /= sum_gamma
+            pi = gamma[0]
 
-
-            for t in range(1, T-1):
-                alpha_t = alpha_t.dot(model.a) * (model.b[:, data[t]])
-                do(t)
-                sum_gamma += gamma_t
-
-            alpha_t = alpha_t.dot(model.a) * (model.b[:, data[-1]])
-            assert abs(alpha_t.sum() - p) < eps_check, \
-                "alpha_t.sum() = {}  !=  p = {}".format(alpha_t.sum(), p)
-
-            sum_gamma = sum_gamma[:, np.newaxis]
-            model = HMMModel(a / sum_gamma, b / sum_gamma, pi)
-            p = self.observation_probability(model, data)
-            return p
+            return HMMModel(a, b, pi), gamma
 
         n, m, T = self.n, len(set(data)), len(data)
         model = start_model
@@ -191,14 +198,15 @@ class HMM():
             pi /= pi.sum()
             model = HMMModel(a, b, pi)
 
-        p_prev, p_curr = 0., self.observation_probability(model, data)
-
+        prev_p, p = -1., 0.
         n_iter = 0
-        while abs(p_curr - p_prev) > eps and n_iter < max_iter:
-            p_curr, p_prev = em(p_curr), p_curr
+        while abs(p - prev_p) > eps and n_iter < max_iter:
+            alpha, beta = self.forward(model, data), self.backward(model, data)
+            model, gamma = update_params(alpha, beta)
+            p = alpha[-1].sum()
             n_iter += 1
 
-        return p_curr, model
+        return p, model
 
     def optimal_model(self, data, n_starts=15, start_model=None, **kwargs):
 
