8eadd5c0d94e9269d9c78d4e945aea880974f31a
atanna <anne.atamanova@gmail.com>
2015-01-09 22:08:25 +0400

    renaming log_values

--- hmm_/hmm.py
+++ hmm_/hmm.py
@@ -19,11 +19,11 @@ class HMMModel():
             y_0 ... y_(m-1) - alphabet for observation data
             q_0 ... g_(T-1) - current hidden states
         a: shape (n,n), transition matrix for hidden states
-        a[i,j] = log_p(q_(t+1)=x_j| q_t=x_i)
+        a[i,j] = p(q_(t+1)=x_j| q_t=x_i)
         b: shape (n,m), log_probability hidden_state -> state
-        b[j,k] = log_p(y_k| x_j)
-        pi: shape (n,), start log_probabilities for hidden states
-        pi_j = log_p(q_0=x_j)
+        b[j,k] = p(y_k| x_j)
+        pi: shape (n,), start probabilities for hidden states
+        pi_j = p(q_0=x_j)
     """
 
     @staticmethod
@@ -34,10 +34,11 @@ class HMMModel():
         return HMMModel.get_model(np.log(a), np.log(b), np.log(pi), **kwargs)
 
     @staticmethod
-    def get_model(a, b, pi, canonic=True):
+    def get_model(log_a, log_b, log_pi, canonic=True):
         model = HMMModel()
-        model.a, model.b, model.pi = np.array(a), np.array(b), np.array(pi)
-        model.n, model.m = model.b.shape
+        model.log_a, model.log_b, model.log_pi = \
+            np.array(log_a), np.array(log_b), np.array(log_pi)
+        model.n, model.m = model.log_b.shape
         model.check()
         if canonic:
             model.canonical_form()
@@ -46,9 +47,9 @@ class HMMModel():
     @staticmethod
     def get_freq_model_and_freq_states(data, h_states, n=None, m=None):
         if n is None:
-            n = len(set(data))
+            n = len(set(h_states))
         if m is None:
-            m = len(set(h_states))
+            m = len(set(data))
         h_state_freq = Counter(h_states)
         b_freq = Counter(zip(h_states, data))
         b_ = np.array([[b_freq[(i, j)] / h_state_freq[i] for j in range(m)]
@@ -65,21 +66,23 @@ class HMMModel():
 
     def canonical_form(self):
         order = np.lexsort(
-            np.round(np.exp(self.b.T), 5))  # admissible error 1e-5
-        self.a = self.a[order, :][:, order]
-        self.b = self.b[order]
-        self.pi = self.pi[order]
+            np.round(np.exp(self.log_b.T), 5))  # admissible error 1e-5
+        self.log_a = self.log_a[order, :][:, order]
+        self.log_b = self.log_b[order]
+        self.log_pi = self.log_pi[order]
 
     def check(self, eps=1e-5):
-        if self.a.shape != (len(self.pi), self.b.shape[0]):
+        if self.log_a.shape != (len(self.log_pi), self.log_b.shape[0]):
             raise HMMException("wrong model's shape")
-        if abs(np.exp(logsumexp(self.a, axis=1)) - np.ones(
-                len(self.a))).max() > eps:
-            raise HMMException("exp_a.sum(axis=1) is not unit vector")
-        if abs(np.exp(logsumexp(self.b, axis=1)) - np.ones(
-                len(self.a))).max() > eps:
+        if abs(np.exp(logsumexp(self.log_a, axis=1)) - np.ones(
+                len(self.log_a))).max() > eps:
+            raise HMMException("{} is not unit vector".format(
+                np.exp(logsumexp(self.log_a, axis=1))))
+        if abs(np.exp(logsumexp(self.log_b, axis=1)) - np.ones(
+                len(self.log_a))).max() > eps:
             raise HMMException("exp_b.sum(axis=1) is not unit vector")
-        if abs(np.exp(logsumexp(self.pi)) - np.ones(len(self.a))).max() > eps:
+        if abs(np.exp(logsumexp(self.log_pi)) - np.ones(
+                len(self.log_a))).max() > eps:
             raise HMMException("exp_sum(pi) must equal 1")
 
     def sample(self, size):
@@ -87,16 +90,17 @@ class HMMModel():
         states = np.arange(self.m)
         data = np.zeros(size, dtype=np.int)
         h_states = np.zeros(size, dtype=np.int)
-        p = self.pi
+        p = self.log_pi
         for i in range(size):
             h_state = stats.rv_discrete(name='custm',
                                         values=(hidden_states,
                                                 np.exp(p))).rvs()
-            p = self.a[h_state]
+            p = self.log_a[h_state]
             data[i] = stats.rv_discrete(name='custm',
                                         values=(states,
                                                 np.exp(
-                                                    self.b[h_state]))).rvs()
+                                                    self.log_b[
+                                                        h_state]))).rvs()
             h_states[i] = h_state
 
         return data, h_states
@@ -108,18 +112,19 @@ class HMMModel():
         def arr_freq(arr):
             return np.exp(arr) * freq
 
-        return {"a": chisquare(arr_freq(self.a), arr_freq(model.a), axis=1),
-                "b": chisquare(arr_freq(self.b), arr_freq(model.b), axis=1)}
+        return {
+        "a": chisquare(arr_freq(self.log_a), arr_freq(model.log_a), axis=1),
+        "b": chisquare(arr_freq(self.log_b), arr_freq(model.log_b), axis=1)}
 
     def distance(self, model):
-        return np.mean([abs(np.exp(self.a) - np.exp(model.a)).mean(),
-                        abs(np.exp(self.b) - np.exp(model.b)).mean()])
+        return np.mean([abs(np.exp(self.log_a) - np.exp(model.log_a)).mean(),
+                        abs(np.exp(self.log_b) - np.exp(model.log_b)).mean()])
 
     def __str__(self):
         return "a: {}\n" \
                "b: {}\n" \
-               "pi: {}".format(np.exp(self.a), np.exp(self.b),
-                               np.exp(self.pi))
+               "pi: {}".format(np.exp(self.log_a), np.exp(self.log_b),
+                               np.exp(self.log_pi))
 
 
 class HMM():
@@ -137,8 +142,8 @@ class HMM():
         :return: p(data| model)
 
         """
-        p = logsumexp(HMM.forward(model, data)[-1])
-        return -np.inf if p != p else p  # fix nan
+        log_p = logsumexp(HMM.forward(model, data)[-1])
+        return -np.inf if log_p != log_p else log_p  # fix nan
 
     @staticmethod
     def optimal_state_sequence(model, data):
@@ -154,18 +159,18 @@ class HMM():
         :return: optimal q_1 ... q_T
         """
 
-        delta_t = model.pi + model.b[:, data[0]]
+        log_delta_t = model.log_pi + model.log_b[:, data[0]]
         psi = []
 
         for d in data[1:]:
-            tmp = delta_t[:, np.newaxis] + model.a
+            tmp = log_delta_t[:, np.newaxis] + model.log_a
             psi.append(tmp.argmax(axis=0))
-            delta_t = tmp.max(axis=0) + model.b[:, d]
+            log_delta_t = tmp.max(axis=0) + model.log_b[:, d]
 
         T = len(data)
         psi = np.array(psi)
         q = np.zeros(T)
-        q[T - 1] = delta_t.argmax()
+        q[T - 1] = log_delta_t.argmax()
         for i in range(2, T + 1):
             q[-i] = psi[-i + 1][q[-i + 1]]
         return q.astype(np.int)
@@ -181,14 +186,14 @@ class HMM():
         :return: alpha
         """
         T = len(data)
-        alpha = np.zeros((T, model.n))
+        log_alpha = np.zeros((T, model.n))
 
-        alpha[0] = model.pi + model.b[:, data[0]]
+        log_alpha[0] = model.log_pi + model.log_b[:, data[0]]
         for t in range(1, T):
-            tmp = logsumexp(alpha[t - 1] + model.a.T, axis=1)
+            tmp = logsumexp(log_alpha[t - 1] + model.log_a.T, axis=1)
             tmp = np.array(list(map(lambda t: -np.inf if t != t else t, tmp)))
-            alpha[t] = tmp + model.b[:, data[t]]
-        return alpha
+            log_alpha[t] = tmp + model.log_b[:, data[t]]
+        return log_alpha
 
     @staticmethod
     def backward(model, data):
@@ -201,14 +206,15 @@ class HMM():
         :return: beta
         """
         T = len(data)
-        beta = np.zeros((T, model.n))
-        beta[T - 1] = np.log(1.)
+        log_beta = np.zeros((T, model.n))
+        log_beta[T - 1] = np.log(1.)
         for t in range(T - 2, -1, -1):
-            beta[t] = logsumexp(
-                model.a + (model.b[:, data[t + 1]] + beta[t + 1]), axis=1)
-        return beta
+            log_beta[t] = logsumexp(
+                model.log_a + (model.log_b[:, data[t + 1]] + log_beta[t + 1]),
+                axis=1)
+        return log_beta
 
-    def _optimal_model(self, data, start_model=None, m=None, eps=1e-30,
+    def _optimal_model(self, data, start_model=None, m=None, log_eps=1e-30,
                        max_iter=1e5):
         """
         Baum-Welch algorithm
@@ -218,41 +224,45 @@ class HMM():
         :return: optimal hmm_model
         """
 
-        def update_params(alpha, beta):
+        def update_params(log_alpha, log_beta):
             """
             additional values: ksi, gamma
             ksi_t[i,j] = p(q_t=x_i, q_(t+1)=x_j|data, model)
             gamma[t,i] = p(q_t = x_i|data, modal)
-            :param alpha:
-            :param beta:
+            :param log_alpha:
+            :param log_beta:
             :return:
             """
-            a = np.log(np.zeros((n, n)))
-            b = np.log(np.zeros((n, m)))
-            gamma = np.log(np.zeros((T, n)))
+            log_a = np.log(np.zeros((n, n)))
+            log_b = np.log(np.zeros((n, m)))
+            log_gamma = np.log(np.zeros((T, n)))
 
-            p = logsumexp(alpha[-1])
+            log_p = logsumexp(log_alpha[-1])
             eps_check = 1e-4
             for t in range(T - 1):
-                ksi_t = alpha[t][:, np.newaxis] + model.a \
-                        + model.b[:, data[t + 1]] + beta[t + 1] - p
-                gamma[t] = logsumexp(ksi_t, axis=1)
+                log_ksi_t = log_alpha[t][:, np.newaxis] + model.log_a \
+                            + model.log_b[:, data[t + 1]] \
+                            + log_beta[t + 1] - log_p
+                log_gamma[t] = logsumexp(log_ksi_t, axis=1)
 
-                a = logsumexp([a, ksi_t], axis=0)
-                b[:, data[t]] = logsumexp([b[:, data[t]], gamma[t]], axis=0)
+                log_a = logsumexp([log_a, log_ksi_t], axis=0)
+                log_b[:, data[t]] = logsumexp(
+                    [log_b[:, data[t]], log_gamma[t]], axis=0)
 
-                assert abs(np.exp(logsumexp(alpha[t] + beta[t])) -
-                           np.exp(p)) < eps_check, \
-                    "{} != {}".format(logsumexp(alpha[t] + beta[t]), p)
-                assert abs(np.exp(logsumexp(gamma[t])) - 1.) < eps_check, \
-                    "{} != 1.".format(np.exp(logsumexp(gamma[t])))
+                assert abs(np.exp(logsumexp(log_alpha[t] + log_beta[t])) -
+                           np.exp(log_p)) < eps_check, \
+                    "{} != {}".format(logsumexp(log_alpha[t] + log_beta[t]),
+                                      log_p)
+                assert abs(np.exp(logsumexp(log_gamma[t])) - 1.) < eps_check, \
+                    "{} != 1.".format(np.exp(logsumexp(log_gamma[t])))
 
-            sum_gamma = logsumexp(gamma, axis=0)[:, np.newaxis]
-            a -= sum_gamma
-            b -= sum_gamma
-            pi = gamma[0]
+            log_sum_gamma = logsumexp(log_gamma, axis=0)[:, np.newaxis]
+            log_a -= log_sum_gamma
+            log_b -= log_sum_gamma
+            log_pi = log_gamma[0]
 
-            return HMMModel.get_model(a, b, pi, canonic=False), gamma
+            return HMMModel.get_model(log_a, log_b, log_pi,
+                                      canonic=False), log_gamma
 
         n, T = self.n, len(data)
         if m is None:
@@ -266,25 +276,28 @@ class HMM():
             pi /= pi.sum()
             model = HMMModel.get_model_from_real_prob(a, b, pi, canonic=False)
 
-        prev_p, p = 1., np.log(0.)
+        log_prev_p, log_p = 1., np.log(0.)
         n_iter = 0.
-        while (prev_p > 0 or (p - prev_p > eps)) and n_iter < max_iter:
-            prev_p = p
-            alpha, beta = self.forward(model, data), self.backward(model, data)
-            model, gamma = update_params(alpha, beta)
-            p = logsumexp(alpha[-1])
+        while (log_prev_p > 0 or (log_p - log_prev_p > log_eps)) \
+                and n_iter < max_iter:
+            log_prev_p = log_p
+            log_alpha = self.forward(model, data)
+            log_beta = self.backward(model, data)
+            model, log_gamma = update_params(log_alpha, log_beta)
+            log_p = logsumexp(log_alpha[-1])
             n_iter += 1
 
-        return p, model
+        return log_p, model, log_gamma
 
     def optimal_model(self, data, n_starts=15, start_model=None, **kwargs):
 
-        best_p, best_model = self._optimal_model(data, start_model, **kwargs)
+        best_log_p, best_model, best_log_gamma = \
+            self._optimal_model(data, start_model, **kwargs)
         for i in range(n_starts):
-            p, model = self._optimal_model(data, **kwargs)
-            if p > best_p:
-                best_p, best_model = p, model
+            log_p, model, log_gamma = self._optimal_model(data, **kwargs)
+            if log_p > best_log_p:
+                best_log_p, best_model, best_log_gamma = log_p, model, log_gamma
         best_model.canonical_form()
-        return best_p, best_model
+        return best_log_p, best_model, best_log_gamma.argmax(axis=1)
 
 
--- hmm_/test_hmm.py
+++ hmm_/test_hmm.py
@@ -6,16 +6,17 @@ def good_test():
     data = np.array([0, 1, 2, 2, 1, 0, 0, 1, 2, 2, 1, 0, 0, 1, 2])
     print("data  ", data)
     for i in range(2, 8):
-        p, model = HMM(i).optimal_model(data, n_starts=5,
-                                        eps=1e-17, max_iter=1e2)
-        print("hmm({}): {}".format(i, p))
+        p, model, h_states = HMM(i).optimal_model(data, n_starts=1,
+                                                  log_eps=1e-7, max_iter=1e2)
+        print("hmm({}): {}".format(i, np.exp(p)))
         if abs(p - 1.) < 1e-8:
             break
-    print("states", HMM(i).optimal_state_sequence(model, data))
+    print("h_states    ", HMM(i).optimal_state_sequence(model, data))
+    print("gamma.argmax", h_states)
     print("model\n{}\n".format(model))
 
 
-def test_sample(model, T):
+def test_sample(model, T, f_name=None, **kwargs):
     """
     sample data from model,
     count optimal_model
@@ -26,40 +27,38 @@ def test_sample(model, T):
     model.canonical_form()
     data, h_states = model.sample(T)
     print("model:\n", model)
-    print("p(data|model)",
-          HMM.observation_log_probability(model, data))
+    log_p = HMM.observation_log_probability(model, data)
+    print("p(data|model)", log_p)
+
+    if f_name is not None:
+        with open(f_name, "w") as f:
+            f.write(
+                "{}\n{}\n{}\n{}\n{}\n".format(T, log_p, model, data, h_states))
 
     freq_model, h_states_freq = \
-        HMMModel.get_freq_model_and_freq_states(data, h_states)
+        HMMModel.get_freq_model_and_freq_states(data, h_states, m=m)
     print("freq_model:\n", freq_model)
     print("p(data|freq_model)",
           HMM.observation_log_probability(freq_model, data))
+    print("h_states_freq:", h_states_freq)
     print("chisquare(model, freq_model):\n",
           model.chisquare(freq_model, h_states_freq))
 
-    p, optimal_model = HMM(n).optimal_model(data, m=m, n_starts=5,
-                                            eps=1e-23, max_iter=1e2)
+    log_p, optimal_model, h_opt_states = HMM(n).optimal_model(data, m=m,
+                                                              **kwargs)
+
+    if f_name is not None:
+        with open(f_name, 'a') as f:
+            f.write("optimal:\n{}\n{}\nh_states{}"
+                    .format(log_p, optimal_model, h_opt_states))
 
     print("optimal_model:\n", optimal_model)
-    print("p(data|optimal_model)", p)
+    print("p(data|optimal_model)", log_p)
     print("chisquare(model, optimal_model):\n",
           model.chisquare(optimal_model, h_states_freq))
     print("dist:", model.distance(optimal_model))
     print("chisquare(freq_model, optimal_model):\n",
           freq_model.chisquare(optimal_model, h_states_freq))
-
-
-def fudge_test_sample():
-    """
-    p(data| model) = 1
-    but distance between models = inf
-    (because permutation of hidden sates is not invariant for model)
-    """
-    n, T = 3, 20
-    data = np.array([0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1])
-    p, model = HMM(n).optimal_model(data, n_starts=5,
-                                    eps=1e-17, max_iter=1e2)
-    test_sample(model, T)
 
 
 def random_test_sample():
@@ -77,6 +76,18 @@ def random_test_sample():
     test_sample(model, T)
 
 
+def fudge_test_sample():
+    """
+    p(data| model) = 1.
+    (because permutation of hidden sates is not invariant for model)
+    """
+    n, T = 3, 20
+    data = np.array([0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1])
+    kwargs = {"n_starts": 5, "log_eps": 1e-17, "max_iter": 1e2}
+    log_p, model, h_states = HMM(n).optimal_model(data, **kwargs)
+    test_sample(model, T, **kwargs)
+
+
 def good_fudge_test_sample():
     n, T = 2, 20
     a = np.array([[0., 0., 1.],
@@ -87,19 +98,20 @@ def good_fudge_test_sample():
                   [0., 0., 0., 1.]])
     pi = np.array([1., 0., 0.])
     model = HMMModel.get_model_from_real_prob(a, b, pi)
-    test_sample(model, T)
+    kwargs = {"n_starts": 5, "log_eps": 1e-3, "max_iter": 1e2}
+    test_sample(model, T, **kwargs)
 
 
 def _test_sample():
-    n, T = 2, int(10e2)
-    # T = 10
+    n, T = 2, int(1e4)
+    print("T", T)
     a = np.array([[0.5, 0.5],
                   [0.3, 0.7]])
     b = np.array([[0.1, 0.9],
                   [0.3, 0.7]])
     pi = np.array([1., 0.0])
     model = HMMModel.get_model_from_real_prob(a, b, pi)
-    test_sample(model, T)
+    test_sample(model, T, n_starts=1, log_eps=2 * 1e-3, max_iter=2 * 1e2)
 
 
 _test_sample()
