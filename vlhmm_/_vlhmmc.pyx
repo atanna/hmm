35b53c4924447e19ecd230dd7426b694fde5781c
atanna <anne.atamanova@gmail.com>
2015-05-08 12:43:46 +0400

    del logaddexp

--- vlhmm_/_vlhmmc.pyx
+++ vlhmm_/_vlhmmc.pyx
@@ -20,21 +20,20 @@ def _log_forward(list contexts,
             log_alpha[t][i] = \
                 log_context_p[i] + \
                 log_b[t, state_c[i]]
+        tmp = np.zeros(log_a.shape[1])
         cdef float log_transition
         for t in range(T - 1):
             for i in range(n_contexts):
                 log_alpha[t+1, i] = np.log(0.)
                 c = contexts[i]
+                tmp.fill(np.log(0.))
                 for c_ in tr_trie.get_list_c(c[1:]):
                     i_ = id_c[c_]
                     log_transition = log_a[state_c[i], i_]
                     if len(c_) > t:
                         log_transition = tr_trie.log_tr_p(c[0], c_[:t+1])
-                    log_alpha[t+1, i] = np.logaddexp(
-                        log_alpha[t+1, i],
-                        log_alpha[t, i_]
-                        + log_transition
-                        + log_b[t + 1, state_c[i]])
+                    tmp[i_] = log_alpha[t, i_] + log_transition
+                log_alpha[t+1, i] = logsumexp(tmp) + log_b[t + 1, state_c[i]]
 
 
 def _log_backward(list contexts,
@@ -51,18 +50,17 @@ def _log_backward(list contexts,
     cdef int n_contexts = log_a.shape[1]
     cdef int t, i, i_, q
     cdef str c, c_
+    tmp = np.zeros(log_a.shape[1])
     for t in range(T - 2, -1, -1):
         for i in range(n_contexts):
             log_beta[t][i] = np.log(0.)
             c = contexts[i]
+            tmp.fill(np.log(0.))
             for q in range(n):
                 for c_ in tr_trie.get_list_c(str(q) + c):
                     i_ = id_c[c_]
-                    log_beta[t, i] = np.logaddexp(
-                        log_beta[t, i],
-                        log_a[q, i]
-                        + log_b[t+1, q]
-                        + log_beta[t+1, i_])
+                    tmp[i_] = log_a[q, i] + log_b[t+1, q] + log_beta[t+1, i_]
+            log_beta[t, i] = logsumexp(tmp)
 
 
 def _log_ksi(list contexts,
@@ -80,18 +78,16 @@ def _log_ksi(list contexts,
     cdef int n_contexts = log_a.shape[1]
     cdef int t, i, q, i_
     cdef str c_
+    tmp = np.zeros(log_a.shape[1])
     for t in range(T - 1):
         for i in range(n_contexts):
             for q in range(n):
-                log_ksi[t][q, i] = np.log(0.)
+                tmp.fill(np.log(0.))
                 for c_ in tr_trie.get_list_c(str(q)+contexts[i]):
                     i_ = id_c[c_]
-                    log_ksi[t, q, i] = \
-                        np.logaddexp(log_ksi[t, q, i],
-                                     log_alpha[t, i] +
-                                     log_a[q, i] +
-                                     log_b[t+1, q] +
-                                     log_beta[t+1, i_])
+                    tmp[i_] = log_alpha[t, i] + log_a[q, i] + \
+                              log_b[t+1, q] + log_beta[t+1, i_]
+                log_ksi[t, q, i] = logsumexp(tmp)
     log_ksi -=  \
         logsumexp(log_ksi, axis=(1, 2)).reshape((T, 1, 1))
 
